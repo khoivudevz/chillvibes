65efe35c689982c15d8863675793ab2088d0b109
itsjinhkz <itsjinhkz@gmail.com>
2022-05-31 20:31:25 +0700

    change loading animation

--- src/Components/Loading/Loading.jsx
+++ src/Components/Loading/Loading.jsx
@@ -1,10 +1,17 @@
 import React from "react";
-import ReactLoading from "react-loading";
+import { Player } from "@lottiefiles/react-lottie-player";
+import loading from "../../assets/loading/loading.json";
 
 export default function Loading() {
   return (
     <div className="fixed h-screen w-screen bg-grey flex items-center justify-center">
-      <ReactLoading type="bars" color="#f36f45" />
+      <Player
+        autoplay
+        loop
+        mode="normal"
+        src={loading}
+        style={{ height: "300px", width: "300px" }}
+      ></Player>
     </div>
   );
 }
